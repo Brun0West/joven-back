584a1958c88c751573ef42d38dcbf4c854108e3c
Bruno Pumapillo Sarmiento <122190257+Brunitozz@users.noreply.github.com>
2025-01-29 17:42:33 -0500

    Add models for Categoria and Mensaje with initial migration

--- buzon/models.py
+++ buzon/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nombre_tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_tipo
+
+class Mensaje(models.Model):
+    id_escrito = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='imagenes/', blank=True, null=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
