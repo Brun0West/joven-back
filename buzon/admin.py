96839d245526c487fcc28a80ff7f55eabeed9c05
Bruno Pumapillo Sarmiento <122190257+Brunitozz@users.noreply.github.com>
2025-01-30 01:40:29 -0500

    Register Mensaje and Categoria models in the Django admin with custom admin classes

--- buzon/admin.py
+++ buzon/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mensaje, Categoria
+
+admin.site.site_header = 'Administracion de Las Penas de Bruno'
+
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id_categoria', 'nombre_tipo')
+    search_fields = ('nombre_tipo',)
+
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ('id_escrito', 'titulo', 'id_categoria', 'fecha')
+    list_display_links = ('titulo', 'id_categoria', 'fecha')
+    search_fields = ('titulo', 'id_categoria', 'fecha')
+    list_filter = ('id_categoria', 'fecha')
+
+admin.site.register(Mensaje, MensajeAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
